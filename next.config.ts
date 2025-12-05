21147251fbd6c829bd436307b7f08cefb8096946
julius <hjulius742002@gmail.com>
2025-12-05 22:49:06 +0800

    Downgrade prisma and prisma-client to 6.6.0 and change how PrismaPlugin import inside next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 const UPLOADTHING_APP_ID = process.env.UPLOADTHING_APP_ID
 
