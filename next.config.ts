33929410cd85b0bd96c57cb4b7cb2321060fb311
julius <hjulius742002@gmail.com>
2025-12-05 23:04:46 +0800

    Back to version Change sw.js icon to user profile

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,9 @@
 import type { NextConfig } from "next";
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 const UPLOADTHING_APP_ID = process.env.UPLOADTHING_APP_ID
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {},
-  webpack: (config, { isServer }) => {
-    // Only apply the plugin to the server-side bundles
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-    return config;
-  },
   images: {
     remotePatterns: [
       {
