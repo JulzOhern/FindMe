cc4288478832c6487c76205a3901576912c73f13
julius <hjulius742002@gmail.com>
2025-12-05 16:25:20 +0800

    Just added the webpack fix inside next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 
 const UPLOADTHING_APP_ID = process.env.UPLOADTHING_APP_ID
@@ -16,7 +17,13 @@ const nextConfig: NextConfig = {
         pathname: "/f/*",
       },
     ]
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
